cd1279921fd5a521e7ac9471b276b7053f9d1dd8
HISA WAY <z8mcb008@gmail.com>
2020-01-28 18:26:51 +0900

    Update benchmarks

--- bench/logistic_map_bench.exs
+++ bench/logistic_map_bench.exs
@@ -2,19 +2,50 @@ defmodule LogisticMapBench do
   use Benchfella
 
   @range_0x1000 (1..0x1000)
+  @stages System.schedulers_online()
+
+  setup_all do
+    IO.inspect @stages
+    {:ok, []}
+  end
 
   bench "Enum" do
     @range_0x1000
     |> PelemaySample.enum_logistic_map
   end
 
-  bench "Flow" do
-    @range_0x1000
-    |> PelemaySample.flow_logistic_map
-  end
+  # bench "HiPE Enum" do
+  #   @range_0x1000
+  #   |> HipeSample.logistic_map
+  # end
 
   bench "Pelemay" do
     @range_0x1000
     |> PelemaySample.logistic_map
+  end
+
+  bench "Flow 32" do
+    @range_0x1000
+    |> PelemaySample.flow_logistic_map(@stages)
+  end
+
+  bench "Flow 1" do
+    @range_0x1000
+    |> PelemaySample.flow_logistic_map(1)
+  end
+
+    bench "Fusion Flow 32" do
+    @range_0x1000
+    |> PelemaySample.fusion_flow_logistic_map(@stages)
+  end
+
+  bench "Fusion Flow 1" do
+    @range_0x1000
+    |> PelemaySample.fusion_flow_logistic_map(1)
+  end
+
+  bench "Stream" do
+    @range_0x1000
+    |> PelemaySample.stream_logistic_map
   end
 end
--- lib/pelemay_sample.ex
+++ lib/pelemay_sample.ex
@@ -2,6 +2,8 @@ defmodule PelemaySample do
   import Pelemay
   require Pelemay
 
+  @compile {:inline, calc: 1}
+
   @moduledoc """
   ```elixir
   defpelemay do
@@ -23,6 +25,11 @@ defmodule PelemaySample do
     def list_minus_2(list) do
       list
       |> Enum.map(&(&1 - 2))
+    end
+
+    def list_plus_1(list) do
+      list
+      |> Enum.map(fn x -> x + 1 end)
     end
 
     def list_plus_2(list) do
@@ -63,33 +70,68 @@ defmodule PelemaySample do
     end
   end
 
+  def calc(x), do: rem(22 * x * (x + 1), 6_700_417) 
+
   def enum_logistic_map(list) do
     list
-    |> Enum.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Enum.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Enum.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Enum.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Enum.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Enum.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Enum.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Enum.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Enum.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Enum.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
+    |> Enum.map(&calc(&1))
+    |> Enum.map(&calc(&1))
+    |> Enum.map(&calc(&1))
+    |> Enum.map(&calc(&1))
+    |> Enum.map(&calc(&1))
+    |> Enum.map(&calc(&1))
+    |> Enum.map(&calc(&1))
+    |> Enum.map(&calc(&1))
+    |> Enum.map(&calc(&1))
+    |> Enum.map(&calc(&1))
   end
 
-  def flow_logistic_map(list) do
+  def fusion_flow_logistic_map(list, stage_num) do
     list
-    |> Flow.from_enumerable()
-    |> Flow.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Flow.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Flow.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Flow.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Flow.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Flow.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Flow.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Flow.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Flow.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
-    |> Flow.map(&rem(22 * &1 * (&1 + 1), 6_700_417))
+    |> Flow.from_enumerable(stages: stage_num)
+    |> Flow.map(& (&1
+      |> calc()
+      |> calc()
+      |> calc()
+      |> calc()
+      |> calc()
+      |> calc()
+      |> calc()
+      |> calc()
+      |> calc()
+      |> calc()
+    ))
     |> Enum.sort()
+  end
+
+  def flow_logistic_map(list, stage_num) do
+    list
+    |> Flow.from_enumerable(stages: stage_num)
+    |> Flow.map(&calc(&1))
+    |> Flow.map(&calc(&1))
+    |> Flow.map(&calc(&1))
+    |> Flow.map(&calc(&1))
+    |> Flow.map(&calc(&1))
+    |> Flow.map(&calc(&1))
+    |> Flow.map(&calc(&1))
+    |> Flow.map(&calc(&1))
+    |> Flow.map(&calc(&1))
+    |> Flow.map(&calc(&1))
+    |> Enum.sort()
+  end
+
+  def stream_logistic_map(list) do
+    list
+    |> Stream.map(&calc(&1))
+    |> Stream.map(&calc(&1))
+    |> Stream.map(&calc(&1))
+    |> Stream.map(&calc(&1))
+    |> Stream.map(&calc(&1))
+    |> Stream.map(&calc(&1))
+    |> Stream.map(&calc(&1))
+    |> Stream.map(&calc(&1))
+    |> Stream.map(&calc(&1))
+    |> Stream.map(&calc(&1))
+    |> Enum.to_list()
   end
 end
